3a408a0b0777dfe52d0b2ca483b23d3cedfe94b2
Francesc Ferre <francesc.ferre@estudiants.urv.cat>
2020-02-27 21:09:33 +0100

    master: added new graphic.

--- ScriptsGrafiques/altresResultats.py
+++ ScriptsGrafiques/altresResultats.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
 
 activities = ['Aprobats', 'Suspesos']
 slices=[70,30]
@@ -7,4 +10,16 @@ plt.pie(slices, labels=activities, colors=colors, startangle=90, shadow=True, ex
 plt.legend()
 plt.title('Percentatge de restaurants Aprobats/Suspesos')
 plt.savefig('../Grafics/restaurantsAprobatsSuspesos.png', bbox_inches='tight')
+plt.show()
+
+freq=[3245740,4096360]
+xlabel=['Buides', 'Plenes']
+freq_series = pd.Series(np.array(freq))
+#plt.figure(figsize=(12,8))
+ax = freq_series.plot(kind='bar')
+ax.set_title('Caselles buides/plenes')
+ax.set_xlabel('Dades')
+ax.set_ylabel('Nombre caselles')
+ax.set_xticklabels(xlabel)
+plt.savefig('../Grafics/puntuacionsBuidesvsPlenes.png', bbox_inches='tight')
 plt.show()
